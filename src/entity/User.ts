59b304a114b6425003774a790df7f86fb529ea32
David Torbeck <davetorbeck@gmail.com>
2018-07-09 23:31:53 -0700

    add jest

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,18 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { Entity, Column, PrimaryColumn, BeforeInsert, BaseEntity } from "typeorm";
 
-@Entity()
-export class User {
+import * as uuidv4 from "uuid/v4";
 
-    @PrimaryGeneratedColumn()
-    id: number;
+@Entity("users")
+export class User extends BaseEntity {
+  @PrimaryColumn("uuid") id: string;
 
-    @Column()
-    firstName: string;
+  @Column("varchar", { length: 255 })
+  email: string;
 
-    @Column()
-    lastName: string;
+  @Column("text") password: string;
 
-    @Column()
-    age: number;
-
+  @BeforeInsert()
+  addId() {
+    this.id = uuidv4();
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-import "reflect-metadata"
-import { GraphQLServer } from "graphql-yoga"
+import "reflect-metadata";
+import { importSchema } from "graphql-import";
+import { GraphQLServer } from "graphql-yoga";
+import { resolvers } from "./resolvers";
+import { createConnection } from "typeorm";
+import * as path from "path";
 
-const typeDefs = `
-    type Query {
-        hello(name: String): String!
-    }
-`
+export const startServer = async () => {
+  const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
+  const server = new GraphQLServer({ typeDefs, resolvers });
 
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || "World"}`
-  }
-}
+  await createConnection();
+  await server.start();
+  console.log("Server is running on localhost:4000");
+};
 
-const server = new GraphQLServer({ typeDefs, resolvers })
-
-server.start(() => console.log("Server is running on localhost:4000"))
+startServer();
